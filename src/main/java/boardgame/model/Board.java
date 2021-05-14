67577e190da8906b9db66a94f24eb60c7dc09321
Tóth Antal <tothantal0326@gmail.com>
2021-05-14 14:08:44 +0200

    Implemented the game board

---
+++ src/main/java/boardgame/model/Board.java
@@ -0,0 +1,245 @@
+package boardgame.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.tinylog.Logger;
+
+public class Board {
+
+    /**
+     * The board is represented as two rows of numbers
+     */
+    private List<Tile> firstRow;
+    private List<Tile> secondRow;
+
+
+    /**
+     * Constructor for custom {@code Board} objects
+     *
+     * @param firstRow	The first row of {@code Tile} objects
+     * @param secondRow	The second row of {@code Tile} objects
+     */
+    public Board(List<Tile> firstRow, List<Tile> secondRow) {
+
+        Logger.info("Creating a new custom board");
+        this.firstRow = firstRow;
+        this.secondRow = secondRow;
+    }
+
+    /**
+     * Creates the default {@code Board} object, representing the game board
+     */
+    public Board() {
+
+        Logger.info("Creating a new board");
+
+        Tile tiles;
+        firstRow = new ArrayList<Tile>();
+        secondRow = new ArrayList<Tile>();
+
+        try {
+            Logger.info("Creating the first row");
+            for(int i = 0; i < 10; i++) {
+                tiles = new Tile(0, new Position(0, i));
+                this.firstRow.add(tiles);
+            }
+            Logger.info("First row created");
+
+            Logger.info("Creating the second row");
+            this.secondRow.add(new Tile(0, new Position(1, 0)));
+            for(int i = 2; i < 10; i++) {
+                tiles = new Tile(i, new Position(1, i - 1));
+                this.secondRow.add(tiles);
+            }
+            this.secondRow.add(new Tile(1, new Position(1, 9)));
+            Logger.info("Second row created");
+        }
+
+        catch (Exception e) {
+            Logger.error(e);
+        }
+    }
+
+    /**
+     * Returns the String representation of the {@code Board} object
+     * String form is: [( num1 num2 num3 ...etc )( num1 num2 num3 ...etc )]
+     *
+     * @return	The String representation
+     */
+    @Override
+    public String toString() {
+
+        Logger.info("Converting Board object to String");
+
+        String firstRowString = "( ";
+        for(int i = 0; i < 10; i++) {
+            firstRowString = firstRowString + firstRow.get(i).toString() + " ";
+        }
+        firstRowString = firstRowString + ")";
+
+        String secondRowString = "( ";
+        for(int i = 0; i < 10; i++) {
+            secondRowString = secondRowString + secondRow.get(i).toString() + " ";
+        }
+        secondRowString = secondRowString + ")";
+
+        return "[" + firstRowString + secondRowString + "]";
+    }
+
+
+    /**
+     * Gets the first row of the game board
+     * @return	the first row of the game board
+     */
+    public List<Tile> getFirstRow() {
+        Logger.info("Getting the first row of the board");
+        return firstRow;
+    }
+
+
+    /**
+     * Sets the first row of the game board
+     * @param firstRow	the first row of the game board
+     */
+    public void setFirstRow(List<Tile> firstRow) {
+        Logger.info("Setting the first row of the board");
+        this.firstRow = firstRow;
+    }
+
+    /**
+     * Gets the second row of the game board
+     * @return	the second row of the game board
+     */
+    public List<Tile> getSecondRow() {
+        Logger.info("Getting the second row of the board");
+        return secondRow;
+    }
+
+
+    /**
+     * Sets the second row of the game board
+     * @param secondRow	the second row of the game board
+     */
+    public void setSecondRow(List<Tile> secondRow) {
+        Logger.info("Setting the second row of the board");
+        this.secondRow = secondRow;
+    }
+
+
+    /**
+     * Checks if the game board is ordered correctly or not
+     *
+     * @return	whether the game board is ordered correctly or not
+     */
+    public boolean isOrdered() {
+
+        String orderedBoardString = "[( 0 0 0 0 0 0 0 0 0 0 )( 1 2 3 4 5 6 7 8 9 0 )]";
+
+        Logger.info("Checking order of Board");
+
+        return this.toString().equals(orderedBoardString);
+    }
+
+    /**
+     * Returns the {@code Tile} at position (X, Y) from {@code Board}
+     *
+     * @param position	The position in question
+     * @return			The Tile at the given position
+     */
+    public Tile at(Position position) {
+
+        Tile result = null;
+
+        Logger.info("Getting tile at " + position.toString());
+
+        try {
+
+            if( (position.getColumn() < 0 || position.getColumn() > 9) ||
+                    (position.getRow() < 0 || position.getRow() > 1 )) {
+                Logger.error("Wrong index: " + position.getColumn() + position.getRow());
+            } else {
+
+                if(position.getRow() == 0) {
+                    return this.firstRow.get(position.getColumn());
+                }
+
+                return this.secondRow.get(position.getColumn());
+            }
+        }
+
+        catch (Exception e) {
+            Logger.error(e);
+        }
+
+        return result;
+    }
+
+    /**
+     * Moves the Tile at position to the given direction
+     *
+     * @param position	the position of the tile
+     * @param direction	the given direction
+     */
+    public void move(Position position, Direction direction) {
+
+        Logger.info("Moving tile at " + position.toString());
+
+        try {
+
+            if(this.at(position).toString().equals("0")) {
+                Logger.error("Cannot move empty tile");
+            } else {
+
+                switch(direction) {
+                    case UP:
+                        if (position.canMoveUp()) {
+                            if(!this.at(position.getUpperNeighbour()).toString().equals("0")) {
+                                Logger.error("Cannot move tile: movement blocked");
+                                break;
+                            }
+                            this.at(position.getUpperNeighbour()).setValue(this.at(position).getValue());
+                            this.at(position).setValue(0);
+                        }
+                        break;
+
+                    case DOWN:
+                        if (position.canMoveDown()) {
+                            if(!this.at(position.getLowerNeighbour()).toString().equals("0")) {
+                                Logger.error("Cannot move tile: movement blocked");
+                                break;
+                            }
+                            this.at(position.getLowerNeighbour()).setValue(this.at(position).getValue());
+                            this.at(position).setValue(0);
+                        }
+                        break;
+
+                    case RIGHT:
+                        if (position.canMoveRight()) {
+                            if(!this.at(position.getRightNeighbour()).toString().equals("0")) {
+                                Logger.error("Cannot move tile: movement blocked");
+                                break;
+                            }
+                            this.at(position.getRightNeighbour()).setValue(this.at(position).getValue());
+                            this.at(position).setValue(0);
+                        }
+                        break;
+
+                    case LEFT:
+                        if (position.canMoveLeft()) {
+                            if(!this.at(position.getLeftNeighbour()).toString().equals("0")) {
+                                Logger.error("Cannot move tile: movement blocked");
+                                break;
+                            }
+                            this.at(position.getLeftNeighbour()).setValue(this.at(position).getValue());
+                            this.at(position).setValue(0);
+                        }
+                }
+            }
+        }
+
+        catch (Exception e) {
+            Logger.error(e);
+        }
+    }
+}
---
+++ src/test/java/boargame/model/BoardTest.java
@@ -0,0 +1,139 @@
+package boargame.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import boardgame.model.Board;
+import boardgame.model.Tile;
+import boardgame.model.Position;
+import boardgame.model.Direction;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import org.tinylog.Logger;
+
+import static org.junit.Assert.*;
+
+
+public class BoardTest {
+
+    Board board;
+
+    @Before
+    public void init() {
+        board = new Board();
+    }
+
+    @Test
+    public void testBoard() {
+
+        String boardState = "[( 0 0 0 0 0 0 0 0 0 0 )( 0 2 3 4 5 6 7 8 9 1 )]";
+
+        assertEquals(boardState, board.toString());
+    }
+
+    @Test
+    public void testBoardWithCustomParameters() {
+
+        List<Tile> testFirstRow = new ArrayList<>();
+        List<Tile> testSecondRow = new ArrayList<>();
+        String testBoardState = "[( 0 1 2 3 4 5 6 7 8 9 )( 0 1 2 3 4 5 6 7 8 9 )]";
+
+        for(int i = 0; i < 10 ; i++) {
+            testFirstRow.add(new Tile(i, new Position(0, i)));
+            testSecondRow.add(new Tile(i, new Position(1, i)));
+        }
+
+        Board testBoard = new Board(testFirstRow, testSecondRow);
+
+        assertEquals(testBoardState, testBoard.toString());
+    }
+
+    @Test
+    public void testIsOrdered() {
+
+        List<Tile> orderedFirstRow = new ArrayList<>();
+        List<Tile> orderedSecondRow = new ArrayList<>();
+
+
+        for(int i = 0; i < 9; i++) {
+            orderedFirstRow.add(new Tile(0, new Position(0, i)));
+            orderedSecondRow.add(new Tile(i + 1, new Position(1, i)));
+        }
+
+        orderedFirstRow.add(new Tile(0, new Position(0, 9)));
+        orderedSecondRow.add(new Tile(0, new Position(0, 9)));
+
+        Board orderedBoard = new Board(orderedFirstRow, orderedSecondRow);
+
+        assertTrue(orderedBoard.isOrdered());
+        assertFalse(board.isOrdered());
+    }
+
+    @Test
+    public void testAt() {
+        assertEquals("0", board.at(new Position(0, 0)).toString());
+        assertEquals("0", board.at(new Position(1, 0)).toString());
+        assertEquals("2", board.at(new Position(1, 1)).toString());
+        assertEquals("3", board.at(new Position(1, 2)).toString());
+    }
+
+    @Test
+    public void testMoveValidMoves() {
+
+        Logger.info("Testing valid movements");
+        board.move(new Position(1, 1), Direction.LEFT);
+        assertEquals(board.toString(), "[( 0 0 0 0 0 0 0 0 0 0 )( 2 0 3 4 5 6 7 8 9 1 )]");
+        board.move(new Position(1, 2), Direction.LEFT);
+        assertEquals(board.toString(), "[( 0 0 0 0 0 0 0 0 0 0 )( 2 3 0 4 5 6 7 8 9 1 )]");
+        board.move(new Position(1, 3), Direction.UP);
+        assertEquals(board.toString(), "[( 0 0 0 4 0 0 0 0 0 0 )( 2 3 0 0 5 6 7 8 9 1 )]");
+        board.move(new Position(0, 3), Direction.DOWN);
+        assertEquals(board.toString(), "[( 0 0 0 0 0 0 0 0 0 0 )( 2 3 0 4 5 6 7 8 9 1 )]");
+        board.move(new Position(1, 7), Direction.UP);
+        assertEquals(board.toString(), "[( 0 0 0 0 0 0 0 8 0 0 )( 2 3 0 4 5 6 7 0 9 1 )]");
+        board.move(new Position(1, 6), Direction.RIGHT);
+        assertEquals(board.toString(), "[( 0 0 0 0 0 0 0 8 0 0 )( 2 3 0 4 5 6 0 7 9 1 )]");
+        board.move(new Position(1, 3), Direction.LEFT);
+        assertEquals(board.toString(), "[( 0 0 0 0 0 0 0 8 0 0 )( 2 3 4 0 5 6 0 7 9 1 )]");
+    }
+
+    @Test
+    public void testMoveNothing() {
+        Logger.info("Testing invalid movements");
+        board.move(new Position(0, 0), Direction.LEFT);
+        board.move(new Position(0, 0), Direction.RIGHT);
+        board.move(new Position(0, 0), Direction.UP);
+        board.move(new Position(0, 0), Direction.DOWN);
+
+        board.move(new Position(1, 0), Direction.LEFT);
+        board.move(new Position(1, 0), Direction.UP);
+        board.move(new Position(1, 0), Direction.DOWN);
+        board.move(new Position(1, 9), Direction.RIGHT);
+
+        assertEquals(board.toString(), "[( 0 0 0 0 0 0 0 0 0 0 )( 0 2 3 4 5 6 7 8 9 1 )]");
+    }
+
+    @Test
+    public void testMoveBlocked() {
+        Logger.info("Testing invalid movements");
+        Logger.info("Another tile in the way");
+        board.move(new Position(1, 9), Direction.LEFT);	// Blocked
+        board.move(new Position(1, 8), Direction.RIGHT); // Blocked
+        board.move(new Position(1, 3), Direction.UP);
+        board.move(new Position(1, 2), Direction.RIGHT);
+        board.move(new Position(0, 3), Direction.DOWN); // Blocked
+        board.move(new Position(1, 3), Direction.UP); // Blocked
+
+        assertEquals(board.toString(), "[( 0 0 0 4 0 0 0 0 0 0 )( 0 2 0 3 5 6 7 8 9 1 )]");
+    }
+
+    @Test
+    public void testMoveZeroValue() {
+        Logger.info("Testing invalid movements");
+        Logger.info("Attempting to move nothing");
+        board.move(new Position(1, 0), Direction.RIGHT);
+        assertEquals(board.toString(), "[( 0 0 0 0 0 0 0 0 0 0 )( 0 2 3 4 5 6 7 8 9 1 )]");
+    }
+}
