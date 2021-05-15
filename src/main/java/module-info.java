6045ea2436892138c72988f633e53e272a51bddb
Tóth Antal <tothantal0326@gmail.com>
2021-05-15 12:44:42 +0200

    Added module info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module Homework.Project {
+
+    requires java.xml.bind;
+
+    requires org.tinylog.api;
+    requires org.tinylog.impl;
+
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires java.activation;
+
+    opens main;
+}
